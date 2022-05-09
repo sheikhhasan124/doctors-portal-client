482c3325254ecfc8487bfabb5b7db71fadf981f1
hasan-ullah <mdhasanullah124@gmail.com>
2022-05-10 05:40:47 +0600

    navbar crate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './pages/about/About';
+import Appointment from './pages/Appointment/Appointment';
+import Login from './pages/Auth/Login';
+import Registration from './pages/Auth/Registration';
+import Contact from './pages/Home/Contact';
+import Home from './pages/Home/Home';
+import Reviews from './pages/Reviews/Reviews';
+import Navber from './pages/Shared/Navber';
+import NotFound from './pages/Shared/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navber/>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/appoinment" element={<Appointment/>}></Route>
+        <Route path="/reviews" element={<Reviews/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="registration" element={<Registration/>}></Route>
+        <Route path="*" element={<NotFound/>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Auth/Social.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Social = () => {
+    return (
+        <div>
+            <h2>social</h2>
+        </div>
+    );
+};
+
+export default Social;
