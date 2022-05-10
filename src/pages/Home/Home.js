3878a2c6d78256968e1c535ae92139b69a37d7a2
hasan-ullah <mdhasanullah124@gmail.com>
2022-05-10 21:13:02 +0600

    testimonial section added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,6 +3,7 @@ import Banner from './Banner';
 import Info from './Info';
 import MakeAppoinment from './MakeAppoinment';
 import Services from './Services';
+import Testimonial from './Testimonial';
 import TreatMentService from './TreatMentService';
 
 const Home = () => {
@@ -13,6 +14,7 @@ const Home = () => {
             <Services/>
             <TreatMentService/>
             <MakeAppoinment/>
+            <Testimonial/>
         </div>
     );
 };
---
+++ src/pages/Home/Testimonial.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import quote from '../../assets/icons/quote.svg'
+import person1 from '../../assets/images/people1.png'
+import person2 from '../../assets/images/people2.png'
+import person3 from '../../assets/images/people3.png'
+import Review from './Review';
+
+const Testimonial = () => {
+    const reviews = [
+        {
+            _id:1,
+            name:'Winson Herry',
+            review:'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            address:'California',
+            img:person1
+        },
+        {
+            _id:2,
+            name:'Wayy Herry',
+            review:'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            address:'Texas',
+            img:person2
+        },
+        {
+            _id:3,
+            name:'Winson Karry',
+            review:'It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content',
+            address:'London',
+            img:person3
+        },
+    ]
+    return (
+        <section>
+            <div className='flex justify-between'>
+                <div>
+                    <h3 className='text-primary text-xl font-bold'>Testimonial</h3>
+                    <h2 className='text-3xl'>What Our Patients Says </h2>
+                </div>
+                <div>
+                    <img src={quote} className="w-24 lg:w-44" alt="" />
+                </div>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                {reviews.map(review=><Review key={review._id} reviewItem={review}></Review>)}
+            </div>
+        </section>
+    );
+};
+
+export default Testimonial;
