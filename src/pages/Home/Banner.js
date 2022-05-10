4a6eecad748c71f79d7af8785d0c11eb49daca55
hasan-ullah <mdhasanullah124@gmail.com>
2022-05-10 17:59:51 +0600

    daisy card addede

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -3,16 +3,19 @@ import chair from '../../assets/images/chair.png'
 
 const Banner = () => {
     return (
-        <div class="hero min-h-screen">
+            <div>
+<div class="hero min-h-screen px-16">
         <div class="hero-content flex-col lg:flex-row-reverse">
-          <img src={chair} class="max-w-lg rounded-lg shadow-2xl" />
-          <div class="pr-7">
-            <h1 class="text-5xl font-bold">Box Office News!</h1>
+          <img src={chair} alt="img" class="max-w-lg rounded-lg shadow-2xl" />
+          <div>
+            <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
             <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <button class="btn btn-primary">Get Started</button>
+            <button class="btn text-white font-bold bg-gradient-to-r from-primary to-secondary btn-primary">Get Started</button>
           </div>
         </div>
       </div>
+            </div>
+        
     );
 };
 
---
+++ src/pages/Home/Info.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import InfoCard from './InfoCard';
+
+
+const Info = () => {
+    return (
+        <div>
+           <InfoCard/> 
+        </div>
+    );
+};
+
+export default Info;
