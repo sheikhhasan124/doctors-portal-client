68af8a599a34847289ede413c45b99ce8b7e194c
hasan-ullah <mdhasanullah124@gmail.com>
2022-05-11 10:36:56 +0600

    available slot create

--- src/pages/Appointment/AvailableAppoinment.js
+++ src/pages/Appointment/AvailableAppoinment.js
@@ -10,9 +10,9 @@ const AvailableAppoinment = ({date}) => {
     .then(data=>setservices(data))
     },[])
     return (
-        <div>
-            <h2 className='text-xl text-primary text-center'>Available Appoinment {format(date,'PP')}</h2>
-            <div>
+        <div className='mt-12'>
+            <h2 className='text-xl text-secondary text-center'>Available Appoinment {format(date,'PP')}</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {services.map(service=><Service key={service._id} service={service}/>)}
             </div>
         </div>
--- src/pages/Appointment/Service.js
+++ src/pages/Appointment/Service.js
@@ -2,13 +2,18 @@ import React from 'react';
 
 const Service = ({service}) => {
     const {name, slot}=service;
+   
     return (
-        <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card la:max-w-lg bg-base-100 shadow-xl text-center">
   <div class="card-body">
-    <h2 class="card-title">{name}</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+    <h2 class="text-xl font-bold text-secondary">{name}</h2>
+    <p>{slot.length>0
+      ? <span>{slot[0]}</span>
+      : <span className='text-red-500'>Try another date</span>
+    }</p>
+    <p>{slot.length} {slot.length>1? 'Spaces':'space'} available</p>
+    <div class="card-actions justify-center">
+      <button disabled={slot.length===0} class="btn-gradient ">Book Appointment</button>
     </div>
   </div>
 </div>
