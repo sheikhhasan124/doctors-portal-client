ad53827e649ffccadfca202d409923664651f990
hasan-ullah <mdhasanullah124@gmail.com>
2022-05-12 11:06:35 +0600

    error handle by form hook

--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -21,33 +21,57 @@ const Login = () => {
 
                 <form onSubmit={handleSubmit(onSubmit)}>
 
-                <div class="form-control w-full max-w-xs">
-                <label class="label">
-                    <span class="label-text">email</span>
+                <div  className="form-control w-full max-w-xs">
+                <label  className="label">
+                    <span  className="label-text">email</span>
                 </label>
                 <input type="email"
                  placeholder="email"
                  {...register("email", 
                  {
+                     required:{
+                       value:true,
+                       message:'email is required'
+                     },
                     pattern: {
-                      value: /[A-Za-z]{3}/,
-                      message: 'error message' // JS only: <p>error message</p> TS only support string
+                      value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                      message: 'provide a valid email' 
                     }
                   }
                  )} 
-                  class="input input-bordered w-full max-w-xs" />
-                <label class="label">
-                    <span class="label-text-alt">Alt label</span> 
+                   className="input input-bordered w-full max-w-xs" />
+                <label  className="label">
+                {errors.email?.type === 'required' && <span  className="label-text-alt text-red-500">{errors.email.message}</span>}
+                {errors.email?.type === 'pattern' && <span  className="label-text-alt text-red-500">{errors.email.message}</span>}
+                     
                 </label>
                 </div>
-
-                
-                {errors.firstName?.type === 'required' && "First name is required"}
-                
-                <input {...register("lastName", { required: true })} />
-                {errors.lastName && "Last name is required"}
-                
-                <input type="submit" />
+                <div  className="form-control w-full max-w-xs">
+                <label  className="label">
+                    <span  className="label-text">password</span>
+                </label>
+                <input type="password"
+                 placeholder="password"
+                 {...register("password", 
+                 {
+                     required:{
+                       value:true,
+                       message:'password is required'
+                     },
+                    minLength: {
+                      value: 6,
+                      message: 'password must be 6' 
+                    }
+                  }
+                 )} 
+                   className="input input-bordered w-full max-w-xs" />
+                <label  className="label">
+                {errors.password?.type === 'required' && <span  className="label-text-alt text-red-500">{errors.password.message}</span>}
+                {errors.password?.type === 'minLength' && <span  className="label-text-alt text-red-500">{errors.password.message}</span>}
+                     
+                </label>
+                </div>
+                 <input className='btn w-full max-w-xs text-white' value="login" type="submit" />
                 </form>
 
                 <div className="divider">OR</div>                
