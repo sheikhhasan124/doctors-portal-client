9ad7c566545a11b54539cd44c592a7b15bb3bcd2
hasan-ullah <mdhasanullah124@gmail.com>
2022-05-17 11:11:34 +0600

    dashboard create

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -4,16 +4,16 @@ import { Link, Outlet } from 'react-router-dom';
 const Dashboard = () => {
     return (
         <div class="drawer drawer-mobile">
-  <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-  <div class="drawer-content flex flex-col items-center justify-center">
+  <input id="dashbord-sidebar" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content ">
     {/* <!-- Page content here --> */}
-    <h2 className='text-2xl'>Dashbord</h2>
+    <h2 className='text-3xl text-orange-500 font-bold'>wellcome to  Dashbord</h2>
     <Outlet/>
-    <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+    
   
   </div> 
   <div class="drawer-side">
-    <label for="my-drawer-2" class="drawer-overlay"></label> 
+    <label for="dashbord-sidebar" class="drawer-overlay"></label> 
     <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
       {/* <!-- Sidebar content here --> */}
       <li><Link to="/dashboard">My Appointment</Link></li>
--- src/pages/dashboard/MyAppointment.js
+++ src/pages/dashboard/MyAppointment.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase_init';
 
 const MyAppointment = () => {
+    const [user]=useAuthState(auth)
+    const [appoinments, setAppoinment]=useState([])
+
+ 
+    useEffect(()=>{
+        if(user){
+            fetch(`http://localhost:5000/booking?patient=${user.email}`)
+        .then(res=>res.json())
+        .then(data=> setAppoinment(data))
+        }
+     },[user])
+   
     return (
         <div>
-            <h2>my appoinments</h2>
+            <h2>my appoinments{appoinments.length}</h2>
+
+            <div class="overflow-x-auto">
+  <table class="table w-full">
+   
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Date</th>
+        <th>Time</th>
+        <th>Treatment</th>
+      </tr>
+    </thead>
+    <tbody>
+     {
+       appoinments.map(a=> <tr>
+        <th>1</th>
+        <td>{a.patientName}</td>
+        <td>{a.date}</td>
+        <td>{a.slot}</td>
+        <td>{a.treatment}</td>
+      </tr>)  
+     }
+       
+     
+    
+    </tbody>
+  </table>
+</div>
         </div>
     );
 };
